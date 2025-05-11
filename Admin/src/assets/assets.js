7911099096b0e87cd16e70f16ef9eabfb90c5bb7
CAO CUONG <21130300@st.hcmuaf.edu.vn>
2025-05-11 22:10:08 +0700

    fix comit Admin

---
+++ Admin/src/assets/assets.js
@@ -0,0 +1,21 @@
+import logo from './logo.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+import home_icon from './home_icon.png'
+import cart_icon from './cart_icon.png'
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon,
+    home_icon,
+    cart_icon
+}
+
+export const url = 'http://localhost:3000'
